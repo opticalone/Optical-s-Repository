e815312fb845c1c5e5e00846145471609a3a8363
s179059 <s179059@SEA-5297F4>
2017-08-14 12:46:00 -0700

    Add project files.

---
+++ 1-HelloAIE/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+int main()
+{
+	//Yo, waddap!
+	std::cout << "Hello AIE" << std::endl;
+	system("pause");
+
+	//Tracklist
+	std::cout << " " << std::endl;
+	std::cout << "Redbone - Come and Get your Love" << std::endl;
+	std::cout << "Anderson .paak - Come Down" << std::endl;
+	std::cout << "Moon Hooch - Number 9" << std::endl;
+	std::cout << "M.I.A. - Paper Planes" << std::endl;
+	std::cout << "Gorillaz - Sleeping Powder" << std::endl;
+	std::cout << " " << std::endl;
+	system("pause");
+
+	//From 0 to 5
+	std::cout << " " << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << " " << std::endl;
+	system("pause");
+
+	//From 5 to 0
+	std::cout << " " << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << " " << std::endl;
+	system("pause");
+
+	//From 0 to 5 to 0
+	std::cout << " " << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << " " << std::endl;
+	system("pause");
+
+	//nothin.
+	// SyntacticalErrors-B)
+	std::cout << "SyntacticalErrors-B)" << std::endl;
+	std::cout << "Four score and seven years ago" << std::endl;
+	std::cout << "our fathers brought forth on this continent," << std::endl;
+	std::cout << "a new nation," << std::endl;
+	std::cout << "conceived in Libery, and dedicated to the proposition" << std::endl;
+	std::cout << "that all men are created equal." << std::endl;
+
+	std::cout << "Now we are engaged in a great civil war," << std::endl;
+	std::cout << "testing whether that nation, or any nation" << std::endl;
+		std::cout << "so conceived and so dedicated, can long endure." << std::endl;
+	// ...
+
+}
